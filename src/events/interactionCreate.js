223c2919a6f414be11e0a4ca38e77b94e24543b6
sl34x <ferit.sleax2003@outlook.com>
2022-04-30 17:43:26 +0300

    lolll

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -14,38 +14,37 @@ export default (Bot) => {
     }
 
     if (interaction.customId === "successTicket") {
-      await interaction.deferReply({ ephemeral: true });
-
       if (
         !Config.TICKET.STAFF_ROLES.some((x) =>
           interaction.member.roles.cache.has(x)
         ) &&
         ![interaction.guild.ownerId].includes(interaction.user.id)
-      )
-        return interaction.followUp({
+      ) {
+        await interaction.deferReply({ ephemeral: true });
+
+        interaction.followUp({
           content: `Only authorities can use the ticket validation system.`,
           ephemeral: true,
         });
+      } else {
+        interaction.message.components[0].components[0].setDisabled(true);
 
-      interaction.message.components[0].components[0].setDisabled(true);
+        interaction.update({
+          components: interaction.message.components,
+        });
 
-      interaction.update({
-        components: interaction.message.components,
-      });
+        interaction.followUp({
+          content: `The ticket has been successfully approved.`,
+          ephemeral: true,
+        });
 
-      interaction.followUp({
-        content: `The ticket has been successfully approved.`,
-        ephemeral: true,
-      });
-
-      interaction.channel
-        .send({
+        interaction.channel.send({
           content: `Heyy! <@!${interaction.channel.name.replace(
             "ticket-",
             ""
           )}>, ticket has been successfully approved by the authorities.`,
-        })
-        .then((msg) => setTimeout(() => msg.delete(), 1000 * 5));
+        });
+      }
     }
 
     if (interaction.customId === "archiveTicket") {
@@ -103,13 +102,7 @@ export default (Bot) => {
     if (interaction.customId === "deleteTicket") {
       await interaction.deferReply({ ephemeral: true });
 
-      const User = interaction.channel.name.replace("ticket-", "");
-
-      if (User !== interaction.user.id)
-        return interaction.followUp({
-          content: `You don't own the ticket.`,
-          ephemeral: true,
-        });
+      let User = interaction.channel.name.replace("ticket-", "");
 
       if (
         !Config.TICKET.STAFF_ROLES.some((x) =>
