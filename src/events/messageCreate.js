13ad02b39da4d955fd96450b260e12ef843aa7a4
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-22 17:37:17 +0300

    a few improvements
    command usability and innovation in some unnecessary parts.

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -8,35 +8,33 @@ export default (Bot) => {
     const Prefix = message.content.toLowerCase().startsWith(Config.PREFIX);
 
     if (!Prefix) return;
+    if (!message.guild) return
 
     const Args = message.content.split(" ").slice(1);
     const Command = message.content.split(" ")[0].slice(Config.PREFIX.length);
 
     if (Command === "ticket") {
-      if (message.author.id !== message.guild.ownerId) return;
-
-      const TicketChannel = message.guild.channels.cache.get(
-        Config.TICKET.CHANNEL
-      );
-
-      message.delete().catch(() => {
-        return undefined;
-      });
-
-      if (TicketChannel) {
-        TicketChannel.send({
-          embeds: [Utils.embed(Config.TICKET.MESSAGE, message.guild, Bot, "")],
-          components: [
-            Utils.button(
-              ButtonStyle.Primary,
-              "Open Ticket!",
-              "🎫",
-              "ticket",
-              false
-            ),
-          ],
-        });
+      if (!message.member.permissions.has("0x0000000000000020")) {
+        return message.reply("You are not permissions to use this command.")
       }
+
+      message.delete();
+
+      let log = message.guild.channels.cache.get(Config.TICKET.CHANNEL)
+      if (!log) return message.reply("Please write ticket channel id in config file.")
+      log.send({
+        embeds: [Utils.embed(Config.TICKET.MESSAGE, message.guild, Bot, "")],
+        components: [
+          Utils.button(
+            ButtonStyle.Primary,
+            "Open Ticket!",
+            "🎫",
+            "ticket",
+            false
+          ),
+        ],
+      });
+      return message.channel.send(`Sended the message to <#${log.id}>`)
     }
   });
 };
