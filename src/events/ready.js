5815b8b9e20b2f8a59429a38892e8f20bd1d526d
thesleax <74549648+thesleax@users.noreply.github.com>
2024-03-31 19:42:48 +0300

    Delete src directory

--- src/events/ready.js
+++
@@ -1,9 +0,0 @@
-import Config from "../config.js";
-
-export default (Bot) => {
-  Bot.on("ready", () => {
-    Bot.user.setActivity(Config.ACTIVITY.NAME, {
-      type: Config.ACTIVITY.TYPE,
-    });
-  });
-};
