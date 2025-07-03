167c589ed3143697c2f2b0227883274738bbf406
Ferit Emre <74549648+thesleax@users.noreply.github.com>
2025-07-03 20:16:33 +0300

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,6 +9,8 @@ const {
   Colors,
 } = Discord;
 import Config from "../config.js";
+import { PermissionFlagsBits, ChannelType, MessageFlags } from "discord.js";
+
 class Utils {
   static login(Bot) {
     Bot.login(Config.TOKEN ? Config.TOKEN : process.env.TOKEN)
@@ -103,6 +105,102 @@ class Utils {
     Modals.addComponents(Row);
 
     return Modals;
+  }
+
+  static createTicketPermissions(userId, staffRoles) {
+    let permissions = [
+      {
+        id: userId,
+        allow: [
+          PermissionFlagsBits.ViewChannel,
+          PermissionFlagsBits.ReadMessageHistory,
+          PermissionFlagsBits.SendMessages,
+        ],
+      },
+      {
+        id: Config.GUILD_ID,
+        deny: [PermissionFlagsBits.ViewChannel],
+      },
+    ];
+
+    staffRoles.forEach((roleId) => {
+      permissions.push({
+        id: roleId,
+        allow: [
+          PermissionFlagsBits.ViewChannel,
+          PermissionFlagsBits.ReadMessageHistory,
+          PermissionFlagsBits.SendMessages,
+        ],
+      });
+    });
+
+    return permissions;
+  }
+
+  static async createTicketChannel(
+    interaction,
+    content = "",
+    hasModalResponse = false
+  ) {
+    const channelName = `ticket-${interaction.user.id}`;
+    const existingChannel = interaction.guild.channels.cache.find(
+      (x) => x.name === channelName
+    );
+
+    if (existingChannel) {
+      await interaction.followUp({
+        content: `You already have a ticket request.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return null;
+    }
+
+    const permissions = this.createTicketPermissions(
+      interaction.user.id,
+      Config.TICKET.STAFF_ROLES
+    );
+
+    const channel = await interaction.guild.channels.create({
+      name: channelName,
+      type: ChannelType.GuildText,
+      parent: Config.TICKET.CATEGORY,
+      permissionOverwrites: permissions,
+    });
+
+    await interaction.followUp({
+      content: "Hey! Your ticket request has been successfully created.",
+      flags: MessageFlags.Ephemeral,
+    });
+
+    const staffMentions = Config.TICKET.STAFF_ROLES.map(
+      (roleId) => `<@&${roleId}>`
+    ).join(", ");
+    const baseContent = `Ticket Creator Member Information: \n${interaction.user} (\`${interaction.user.id}\`)`;
+    const finalContent = hasModalResponse
+      ? `${baseContent}\n${content}`
+      : baseContent;
+
+    await channel.send({
+      content: staffMentions,
+      embeds: [
+        this.embed(
+          finalContent,
+          interaction.guild,
+          interaction.client,
+          interaction.user
+        ),
+      ],
+      components: [this.ticketButton()],
+    });
+
+    return channel;
+  }
+
+  static isStaffMember(member) {
+    return (
+      Config.TICKET.STAFF_ROLES.some((x) => member.roles.cache.has(x)) ||
+      member.id === member.guild.ownerId
+    );
   }
 }
 
